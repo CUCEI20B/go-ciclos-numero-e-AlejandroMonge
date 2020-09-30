b18d756da22ccb905c5950f18deb0faac82b6d81
AlejandroMonge <kmonge_rey@hotmail.com>
2020-09-30 15:16:43 -0500

    Prueba01

--- main.go
+++ main.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+
+	var e float64 = 0.0
+	var ciclo int
+
+	fmt.Scanln(&ciclo)
+
+	for i := 0; i < ciclo; i++ {
+
+		e += float64(1) / float64(factorial(i))
+
+	}
+
+	fmt.Println(e)
+}
+
+func factorial(numero int) int {
+
+	factorial := 1
+
+	for i := 1; i <= numero; i++ {
+		factorial = factorial * i
+	}
+
+	return factorial
 }
